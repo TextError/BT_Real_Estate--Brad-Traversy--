160e7354915ce08ad715b0d5ddb405a697ad50c6
TextError <savinraul1983@yahoo.com>
2019-01-10 12:50:16 +0000

    clear linting // make pagination show 3

--- BT_RealEstate/listings/views.py
+++ BT_RealEstate/listings/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
+
+# Listings
 def index(request):
+  ## Fetch Listing ##
   listings = Listing.objects.all()
 
+  ## Pagination
+  paginator = Paginator(listings, 3)
+  page = request.GET.get('page')
+  page_listings = paginator.get_page(page)
+
+  ## List ##
   context = {
-    'listings': listings
+    'listings': page_listings
   }
   return render(request, 'listings/listings.html', context)
 
+# Listing
 def listing(request, listing_id):
   return render(request, 'listings/listing.html')
 
+# Search
 def search(request):
   return render(request, 'listings/search.html')
