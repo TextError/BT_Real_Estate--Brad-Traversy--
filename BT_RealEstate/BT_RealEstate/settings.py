a8feaf0fd6abe6cd26830913a942a7d8cca176ff
TextError <savinraul1983@yahoo.com>
2019-01-19 12:15:58 +0000

    send email from inquiry

--- BT_RealEstate/BT_RealEstate/settings.py
+++ BT_RealEstate/BT_RealEstate/settings.py
@@ -142,3 +142,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'learntocode2019@gmail.com'
+EMAIL_HOST_PASSWORD = 'learntocode'
+EMAIL_USE_TLS = True
